5964b69169b4951176a0b3f4957e4a7bab69aece
flowerpetal0321 <flowerpetal0321@gmail.com>
2022-07-13 09:04:48 -0700

    changed to global commands

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,7 +3,7 @@
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
-		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+		console.log(`${interaction.user.tag} triggered an interaction.`);
 
         if (!interaction.isCommand()) return;
 
