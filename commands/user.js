36d2fad467ce2979696546e547af7c255436661b
flowerpetal0321 <flowerpetal0321@gmail.com>
2022-07-14 10:56:06 -0700

    added ability to add and delete users

--- commands/user.js
+++ commands/user.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { userInfo } = require('../database-create.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,13 +10,13 @@ module.exports = {
 				.setName('add')
 				.setDescription('Add a user to our list')
 				.addUserOption(option => option.setName('user').setDescription('Pick a user').setRequired(true))
-				.addStringOption(option => option.setName('tetrio_user_id').setDescription('The user ID (NOT the username!!)').setRequired(true)))
+				.addStringOption(option => option.setName('tetrio_id').setDescription('The user ID (NOT the username!!)').setRequired(true)))
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('delete')
 				.setDescription('Delete a user from our list')
 				.addUserOption(option => option.setName('user').setDescription('Pick a user'))
-				.addStringOption(option => option.setName('tetrio_user_id').setDescription('The user ID (NOT the username!!)')))
+				.addStringOption(option => option.setName('tetrio_id').setDescription('The user ID (NOT the username!!)')))
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('leaderboard')
@@ -36,15 +37,52 @@ module.exports = {
 				.setName('who')
 				.setDescription('Find the Discord username or the TETR.IO username when you specify the other thing.')
 				.addUserOption(option => option.setName('user').setDescription('Pick a user'))
-				.addStringOption(option => option.setName('tetrio_user_id').setDescription('The user ID (NOT the username!!)'))),
+				.addStringOption(option => option.setName('tetrio_id').setDescription('The TETR.IO user ID (NOT the username!!)'))),
 		async execute(interaction) {
 			await interaction.deferReply();
 
 			if(interaction.options.getSubcommand() === 'add') {
-				await interaction.editReply('user add: this feature hasn\'t been completed yet lol sorry');
+				const userDiscordID = interaction.options.getUser('user');
+				const userTetrioID = interaction.options.getString('tetrio_id');
+
+				if(userTetrioID.length != 24){
+					return interaction.editReply('That doesn\'t appear to be a TETR.IO user ID :( make sure you copied the user ID, not the username!');
+				}
+				
+				try {
+					const newEntry = await userInfo.create({
+						discordID: userDiscordID,
+						tetrioID: userTetrioID,
+					});
+
+					return interaction.editReply('<@' + newEntry.discordID + '>' + ' was added with the TETR.IO ID ' + newEntry.tetrioID);
+				}
+				catch(error) {
+					if (error.name === 'SequelizeUniqueConstraintError') {
+						return interaction.editReply('It appears that user is already in the list.');
+					}
+					
+					return interaction.editReply('Something went wrong while adding that user :(');
+				}
 			}
 			else if(interaction.options.getSubcommand() === 'delete') {
-				await interaction.editReply('user delete: this feature hasn\'t been completed yet lol sorry');
+				if (interaction.options.getUser('user')) {
+					const userDiscordID = interaction.options.getUser('user');
+					const rowCount = await userInfo.destroy({ where: { discordID: userDiscordID}});
+
+					if (!rowCount) return interaction.editReply('That Discord user does not appear to be in the list.');
+
+					return interaction.editReply('User <@' + userDiscordID + '> has been deleted.');
+				}
+				if (interaction.options.getString('tetrio_id')) {
+					const userTetrioID = interaction.options.getString('tetrio_id');
+					const rowCount = await userInfo.destroy({ where: { tetrioID: userTetrioID}});
+
+					if (!rowCount) return interaction.editReply('That TETR.IO user does not appear to be in the list.');
+
+					return interaction.editReply('TETR.IO User ' + userTetrioID + ' has been deleted.');
+				}
+				return interaction.editReply('bruh lmao');
 			}
 			else if(interaction.options.getSubcommand() === 'leaderboard') {
 				await interaction.editReply('user leaderboard: this feature hasn\'t been completed yet lol sorry');
