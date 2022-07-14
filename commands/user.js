5b4f7a93644d17e16df397801dbc42292ee3c303
flowerpetal0321 <flowerpetal0321@gmail.com>
2022-07-14 08:50:06 -0700

    added the user family of commands

---
+++ commands/user.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('user')
+		.setDescription('Do something with our list of users.')
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('add')
+				.setDescription('Add a user to our list')
+				.addUserOption(option => option.setName('user').setDescription('Pick a user').setRequired(true))
+				.addStringOption(option => option.setName('tetrio_user_id').setDescription('The user ID (NOT the username!!)').setRequired(true)))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('delete')
+				.setDescription('Delete a user from our list')
+				.addUserOption(option => option.setName('user').setDescription('Pick a user'))
+				.addStringOption(option => option.setName('tetrio_user_id').setDescription('The user ID (NOT the username!!)')))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('leaderboard')
+				.setDescription('See our leaderboard!')
+				.addStringOption(option =>
+					option.setName('statistic')
+					.setDescription('Pick which statistic to rank our members by! Defaults to TR.')
+					.addChoices(
+						{ name: 'Tetra League Rating', value: 'tr' },
+						{ name: '40 Lines Sprint', value: '40l' },
+					)))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('list')
+				.setDescription('See the list of users that have been registered on our list'))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('who')
+				.setDescription('Find the Discord username or the TETR.IO username when you specify the other thing.')
+				.addUserOption(option => option.setName('user').setDescription('Pick a user'))
+				.addStringOption(option => option.setName('tetrio_user_id').setDescription('The user ID (NOT the username!!)'))),
+		async execute(interaction) {
+			await interaction.reply('This feature hasn\'t been completed yet lol sorry');
+		}
+};
