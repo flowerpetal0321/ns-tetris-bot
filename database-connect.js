d31e33369d2386c282637ff1dec9892c86eac220
flowerpetal0321 <flowerpetal0321@gmail.com>
2022-07-15 10:40:39 -0700

    moved connection info out of the file

--- database-connect.js
+++ database-connect.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
 const { identity } = require('lodash');
+const { databaseHost } = require('./config.json');
 
 module.exports = {
     // Connecting to the database
     sequelize: new Sequelize('database', 'user', 'password', {
-        host: 'localhost',
+        host: databaseHost,
         dialect: 'sqlite',
         logging: false,
         storage: 'database.sqlite',
